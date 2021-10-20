cd6e68526d7b5128216a98297afd82f6b3de70f5
Cherry <ilovecherries@protonmail.com>
2021-10-20 17:13:05 -0400

    tags i think?

--- src/model.nim
+++ src/model.nim
@@ -3,12 +3,27 @@ import norm/[sqlite, model, pragmas]
 import options
 import std / with
 from sequtils import foldl
+from sugar import collect
 
+proc create*[T: Model](dbConn: DbConn, model: T): T
+  {.raises: [DbError, ValueError].} =
+  ## Inserts the model with the properties and returns it with the newly
+  ## inserted ID
+  var m = model
+  dbConn.insert m
+  let id = dbConn.count(T)
+  m.id = id
+  result = m
+  
 const
   Upvote* = 1
   Downvote* = -1
 
 type
+  DuplicateError* = object of ValueError
+    ## An error that is called when there is already a
+    ## duplicate item in the database.
+  
   User* = ref object of Model
     ## Someone who interacts with the website.
     name* {.unique.}: string
@@ -24,8 +39,23 @@ type
     ## A category which posts can fit into
     name* {.unique.}: string
     ## The name of the category
-    parentID: Option[int64]
+    parentID*: Option[int64]
     ## The parent ID of the category
+
+  TagPostRelationship* = ref object of Model
+    ## Creates a relationship between the posts and the tags that
+    ## are attached to the post
+    postID*: int64
+    ## The ID of the post that is attached to the tag
+    tagID*: int64
+    ## The ID of the tag that is attached to the post
+    
+  Tag* = ref object of Model
+    ## Tags which can be attached to a post
+    name* {.unique.}: string
+    ## The name of the tag
+    color*: string
+    ## Hex-code formatted for display on the website
   
   Post* = ref object of Model
     ## A Post on the website
@@ -35,31 +65,20 @@ type
     ## The title of the post
     categoryID*: int64
     ## The ID of the category that the post is assigned to
-    
-  Tag* = ref object of Model
-    ## Tags which can be attached to a post
-    name* {.unique.}: string
-    ## The name of the tag
-    color*: string
-    ## Hex-code formatted for display on the website
-
-  TagPostRelationship = ref object of Model
-    ## Creates a relationship between the posts and the tags that
-    ## are attached to the post
-    postID: int64
-    ## The ID of the post that is attached to the tag
-    tagID: int64
-    ## The ID of the tag that is attached to the post
+    score*: int64
+    ## A cached calculation of the score that is generated using countScore, can
+    ## also be increased without calculating the score if using the addVote and
+    ## removeVote
 
   Vote* = ref object of Model
     ## Representation of a vote that affects a Post
-    userID: int64
+    userID*: int64
     ## The ID of the user performing the vote
-    postID: int64
+    postID*: int64
     ## The post which the vote is being performed on
-    score: int
-    ## The score leniance of of the vote, an Upvote increases the score
-    ## while a Downvote decreases the score.
+    score*: int
+    ## The score leniance of of the vote, an Upvote (1) increases the score
+    ## while a Downvote (-1) decreases the score.
 
 func newUser*(
   name = "",
@@ -95,7 +114,7 @@ func newCategory*(
     name: name,
     parentID: parentID
   )
-
+  
 func newTag*(
   name = "",
   color = "#000000"
@@ -105,6 +124,118 @@ func newTag*(
     color: color
   )
 
+func newTagPostRelationship*(
+  postID: int64 = 0,
+  tagID: int64 = 0
+): TagPostRelationship =
+  TagPostRelationship(
+    postID: postID,
+    tagID: tagID
+  )
+
+proc addTag*(dbConn: DbConn, post: Post, tag: Tag)
+  {.raises: [DuplicateError, DbError, ValueError].} =
+  ## Creates an association between a post and a tag and adds it to the
+  ## database.
+  # check if a tag association for the post already exists
+  try:
+    var tagPostRelationship = newTagPostRelationship()
+    dbConn.select(
+      tagPostRelationship,
+      "TagPostRelationship.postID = ? AND TagPostRelationship.tagID = ?",
+      post.id,
+      tag.id
+    )
+    raise DuplicateError.newException(
+      "A TagPostRelationship for this post and tag already exist."
+    )
+  except NotFoundError:
+    discard
+  # now create a tag-post relationship and add it to the database
+  discard dbConn.create(newTagPostRelationship(post.id, tag.id))
+
+proc getTags*(dbConn: DbConn, post: Post): seq[Tag]
+  {.raises: [NotFoundError, DbError, ValueError].} =
+  ## Gets all of the tags that are attached to a post
+  var relationships = @[newTagPostRelationship()]
+  dbConn.select relationships, "TagPostRelationship.postID = ?", post.id
+  result = collect(newSeq):
+    for i in relationships:
+      var tag = newTag()
+      dbConn.select tag, "Tag.id = ?", i.tagID
+      tag
+
+proc delete*(dbConn: DbConn, tag: var Tag) =
+  ## A modified version of DbConn.delete for removing all
+  ## tag-post relationships associated with the tag.
+  # delete all of the TagPostRelationships associated with the tag
+  var relationships = @[newTagPostRelationship()]
+  dbConn.select relationships, "TagPostRelationship.tagID = ?", tag.id
+  for i in relationships:
+    var model = i
+    dbConn.delete model
+  sqlite.delete(dbConn, tag)
+
+func newPost*(
+  content = "",
+  title = "",
+  categoryID: int64 = -1,
+): Post =
+  Post(
+    content: content,
+    title: title,
+    categoryID: categoryID
+  )
+  
+proc createPost*(dbConn: DbConn, content: string, title: string,
+                 categoryID: int64, tagIDs: seq[int64]): Post
+  {.raises: [DuplicateError, NotFoundError, DbError, ValueError].} =
+  ## Creates a post with the given properties and adds it to the database,
+  ## then returns the created post with the associated ID
+  # the category should exist in the database, this will throw an error
+  # and exit the function if it cannot find the category
+  var category = newCategory()
+  dbConn.select category, "Category.id = ?", categoryID
+  # we should check if a post with the same title already exists
+  try:
+    var post = newPost()
+    dbConn.select post, "Post.title = ?", title
+    raise DuplicateError.newException(
+      "A post with the title, \"" & title & "\", already exists."
+    )
+  except NotFoundError:
+    discard
+  # now, check if all of the tags listed exist
+  # TODO: DbConn.transaction may help optimize this?
+  var tags = newSeq[Tag]()
+  block:
+    var tag = newTag()
+    for i in tagIDs:
+      try:
+        dbConn.select tag, "Tag.id = ?", i
+        tags.add(tag)
+      except NotFoundError:
+        raise ValueError.newException(
+          "A tag with the ID " & $i & " does not exist."
+        )
+  # create the post and add it to the database
+  var post = dbConn.create(newPost(content, title, categoryID))
+  # now attach all of the tags to the post
+  for tag in tags:
+    dbConn.addTag(post, tag)
+  return post
+
+proc delete*(dbConn: DbConn, post: var Post) =
+  ## A modified version of DbConn.delete for removing all
+  ## tag-post relationships associated with the post.
+  # delete all of the TagPostRelationships associated with the post
+  var relationships = @[newTagPostRelationship()]
+  dbConn.select relationships, "TagPostRelationship.postID = ?", post.id
+  for i in relationships:
+    var model = i
+    dbConn.delete model
+  sqlite.delete(dbConn, post)
+  
 func newVote*(
   userID: int64 = 0,
   postID: int64 = 0,
@@ -117,7 +248,8 @@ func newVote*(
   )
 
 proc addVote*(dbConn: DbConn, user: User, post: Post, score: int)
-  {.raises: [NotFoundError, ValueError, DbError].} =
+  {.raises: [NotFoundError, ValueError, DbError, DuplicateError].} =
+  ## Adds a vote to a post that's attached to a user.
   # check if the score is valid
   if (score != -1) and (score != 1):
     raise ValueError.newException(
@@ -129,12 +261,11 @@ proc addVote*(dbConn: DbConn, user: User, post: Post, score: int)
     dbConn.select vote, "Vote.userID = ? AND Vote.postID = ?", user.id, post.id
     # create an exception if this has the same score
     if vote.score == score:
-      raise ValueError.newException("An identical vote already exists.")
+      raise DuplicateError.newException("An identical vote already exists.")
     vote.score = score
     dbConn.update vote
   except NotFoundError:
-    var vote = newVote(user.id, post.id, score)
-    dbConn.insert vote
+    discard dbConn.create(newVote(user.id, post.id, score))
       
 proc removeVote*(dbConn: DbConn, user: User, post: Post)
   {.raises: [NotFoundError, DbError, ValueError].} =
@@ -144,18 +275,7 @@ proc removeVote*(dbConn: DbConn, user: User, post: Post)
   with dbConn:
     select vote, "Vote.userID = ? AND Vote.postID = ?", user.id, post.id
     delete vote
-    
-func newPost*(
-  content = "",
-  title = "",
-  categoryID = -1,
-): Post =
-  Post(
-    content: content,
-    title: title,
-    categoryID: categoryID
-  )
-
+        
 proc countScore*(dbConn: DbConn, post: Post): int =
   ## Measures the score in the post by adding all of the votes that have
   ## been created for it.
@@ -167,6 +287,7 @@ proc createDatabase*(filename = ":memory"): DbConn =
   result = open(filename, "", "", "")
   result.createTables(newUser())
   result.createTables(newCategory())
+  result.createTables(newTagPostRelationship())
   result.createTables(newTag())
-  result.createTables(newVote())
   result.createTables(newPost())      
+  result.createTables(newVote())
--- tests/tmodel.nim
+++ tests/tmodel.nim
@@ -5,6 +5,48 @@ import std / with
 
 suite "Database/Models":
   let dbConn = createDatabase()
+  suite "Tags":
+    const testTitle = "title"
+    const testTagName = "testtag"
+    var
+      category = dbConn.create(newCategory("Test Category"))
+      post: Post
+      tag = dbConn.create(newTag(testTagName))
+
+    setup:
+      post = dbConn.create(
+        newPost("my content", testTitle, category.id)
+      )
+
+    teardown:
+      dbConn.delete post
+
+    test "Creating a post with a single tag and checking the tag is attached":
+      var
+        post = dbConn.createPost(
+          "the content",
+          "the super cool title",
+          category.id,
+          @[tag.id]
+        )
+        tags = dbConn.getTags(post)
+      dbConn.delete post
+      check tags[0].name == testTagName
+
+    test "Adding a tag to an existing post and checking if it is attached":
+      dbConn.addTag(post, tag)
+      let tags = dbConn.getTags(post)
+      check tags[0].name == testTagName
+      
+    test "Adding a single to a post multiple times should error":
+      dbConn.addTag(post, tag)
+      expect(DuplicateError):
+        dbConn.addTag(post, tag)
+
+    with dbConn:
+      delete tag
+      delete category
+  
   suite "Votes":
     var
       user = newUser()
@@ -16,10 +58,10 @@ suite "Database/Models":
       except NotFoundError:
         discard
     
-    test "Score that is -1 (Downvote)":
+    test "Score that is -1 (Downvote) should be valid":
       dbConn.addVote user, post, Downvote
     
-    test "Score that is 1 (Upvote)":
+    test "Score that is 1 (Upvote) should be valid":
       dbConn.addVote user, post, Downvote
     
     test "Score that is invalid should throw error":
@@ -28,5 +70,41 @@ suite "Database/Models":
         
     test "Should throw error is vote with identical copy is made":
       dbConn.addVote user, post, Upvote
-      expect(ValueError):
+      expect(DuplicateError):
         dbConn.addVote user, post, Upvote
+
+    test "Removing vote that doesn't exist should throw error":
+      expect(NotFoundError):
+        dbConn.removeVote user, post
+      
+    test "Adding positive votes should increase score":
+      var
+        user1 = User(id: 1)
+        user2 = User(id: 2)
+        user3 = User(id: 3)
+      with dbConn:
+        addVote user1, post, Upvote
+        addVote user2, post, Upvote
+        addVote user3, post, Upvote
+      let voteCount = dbConn.countScore(post)
+      with dbConn:
+        removeVote user1, post
+        removeVote user2, post
+        removeVote user3, post
+      check voteCount == 3
+            
+    test "Adding negative votes should decrease score":
+      var
+        user1 = User(id: 1)
+        user2 = User(id: 2)
+        user3 = User(id: 3)
+      with dbConn:
+        addVote user1, post, Downvote
+        addVote user2, post, Downvote
+        addVote user3, post, Downvote
+      let voteCount = dbConn.countScore(post)
+      with dbConn:
+        removeVote user1, post
+        removeVote user2, post
+        removeVote user3, post
+      check voteCount == -3
